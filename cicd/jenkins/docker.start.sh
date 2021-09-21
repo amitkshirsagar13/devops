b54f887245142154a9bbfde789a720694b91346d
amitkshirsagar13 <amit.kshirsagar.13@gmail.com>
2021-09-21 15:47:06 +0530

    added more setup code

--- cicd/jenkins/docker.start.sh
+++ cicd/jenkins/docker.start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker run -d --name jenkins-devops -v $(pwd)/libraries/:/libraries -p 3000:8080 amitkshirsagar13/devops/jenkins:latest
+docker run -d --name jenkins-devops -v $(pwd)/libraries:/libraries -p 3000:8080 amitkshirsagar13/devops/jenkins:latest
 
 
