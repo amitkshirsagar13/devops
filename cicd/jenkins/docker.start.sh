04ed75fab05fbbcceb3d79402b7beceb56c6f9b4
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-10-03 20:45:04 +0530

    jenkins and slave build script tested

--- cicd/jenkins/docker.start.sh
+++ cicd/jenkins/docker.start.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-docker rm -f jenkins-devops
-docker run -d --name jenkins-devops \
--v /var/run/docker.sock:/var/run/docker.sock \
--v /var/jenkins:/var/jenkins_home \
--e CASC_VAULT_FILE=/var/jenkins_home/seed_secrets/jcasc_vault \
--p 5000:5000 -p 3000:8080 -p 50000:50000 \
-amitkshirsagar13/devops/jenkins:latest
+docker-compose up |tee jenkins-master.log
