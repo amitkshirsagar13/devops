57228f8de40a5e54221ddcb9d8489c54d23e1d98
amitkshirsagar13 <amit.kshirsagar.13@gmail.com>
2021-12-09 22:41:28 +0530

    added terraform credentials to jenkins

--- cicd/jenkins/buildJenkins.sh
+++ cicd/jenkins/buildJenkins.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 docker build -t amitkshirsagar13/devops-jenkins .
 docker push amitkshirsagar13/devops-jenkins
-cd slaves
-./slaves/buildSlave.sh
-cd ..
+if [ "$#" -ne 0 ]; then
+  cd slaves
+  ./buildSlave.sh
+  cd ..
+fi
