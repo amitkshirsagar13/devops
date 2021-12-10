f575087c6a20aeef6eb0be66aa90b20b47cd26d3
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-10 18:58:22 +0530

    updated docker agent config

--- cicd/jenkins/slaves/buildSlaves.sh
+++ cicd/jenkins/slaves/buildSlaves.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-docker build -t amitkshirsagar13/devops-jenkins-agent-docker . -f ./Dockerfile.generic.slave
+docker build -t amitkshirsagar13/devops-jenkins-agent-generic . -f ./Dockerfile.generic.slave
 docker build -t amitkshirsagar13/devops-jenkins-agent-maven . -f ./Dockerfile.mvn.slave
+docker build -t amitkshirsagar13/devops-jenkins-agent-terraform . -f ./Dockerfile.terraform.slave
 
-docker push amitkshirsagar13/devops-jenkins-agent-docker
+docker push amitkshirsagar13/devops-jenkins-agent-generic
 docker push amitkshirsagar13/devops-jenkins-agent-maven
+docker push amitkshirsagar13/devops-jenkins-agent-terraform
