d05f9539268d96d8130aa525046f9a3617266c34
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-11 14:24:45 +0530

    added libraries

--- libraries/terraform/steps/terraformApply.groovy
+++ libraries/terraform/steps/terraformApply.groovy
@@ -1,3 +1,10 @@
 void call(action, modulePath) {
   println "Terraform ${action} for ${modulePath}"
+  
+  dir("${modulePath}") {
+    withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
+                      string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
+      sh 'terragrunt apply -var aws_access_key=$AWS_ACCESS_KEY_ID -var aws_access_key=$AWS_SECRET_ACCESS_KEY'
+    }
+  }
 }
