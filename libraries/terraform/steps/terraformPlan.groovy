56b8c3bd730b653147f4eb760498d1bdfbf8f72b
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-11 22:11:10 +0530

    added libraries

--- libraries/terraform/steps/terraformPlan.groovy
+++ libraries/terraform/steps/terraformPlan.groovy
@@ -1,17 +1,10 @@
 void call(module, action, modulePath) {
   stage("Terraform Plan") {
     println "Terraform ${action} for ${modulePath}"
-
-    terragruntCommand = module == "module" ? "terragrunt run-all ":"terragrunt "
-
     dir("${modulePath}") {
       withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
                         string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-        // sh 'terraform plan -var aws_access_key=$AWS_ACCESS_KEY_ID -var aws_access_key=$AWS_SECRET_ACCESS_KEY'
-        sh """
-          pwd
-          ls -ltr
-        """
+        terragruntCommand = module == "module" ? "terragrunt run-all ":"terragrunt "
         sh '$terragruntCommand plan -var aws_access_key=$AWS_ACCESS_KEY_ID -var aws_access_key=$AWS_SECRET_ACCESS_KEY'
       }
     }
