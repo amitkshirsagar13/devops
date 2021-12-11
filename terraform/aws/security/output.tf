cdc02996b1cfa136f6da112be08f095ec3edb9d1
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-11 23:23:08 +0530

    added libraries

--- terraform/aws/security/output.tf
+++ terraform/aws/security/output.tf
@@ -2,6 +2,10 @@ output "sg_id" {
     value = "${aws_security_group.sg.id}"
 }
 
+output "sg_name" {
+    value = "${aws_security_group.sg.name}"
+}
+
 output "sg_arn" {
     value = "${aws_security_group.sg.arn}"
 }
