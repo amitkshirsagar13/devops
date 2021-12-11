d4c312b77cde8eaeb9ea851a84fbc5380ec6e55c
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-11 23:21:34 +0530

    added libraries

--- terraform/aws/security/output.tf
+++ terraform/aws/security/output.tf
@@ -6,10 +6,6 @@ output "sg_arn" {
     value = "${aws_security_group.sg.arn}"
 }
 
-output "sg_region" {
-    value = "${aws_security_group.sg.region}"
-}
-
 output "aws_access_key" {
     value = "${var.aws_access_key}"
 }
