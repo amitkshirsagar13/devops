853739d11dacce2d47858543064a44b49a1cd4aa
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-11 11:42:07 +0530

    added jenkins library

---
+++ libraries/jenkins/steps/userInfo.groovy
@@ -0,0 +1,14 @@
+void call(key) {
+  wrap([$class: 'BuildUser']) {
+    if (key == 'USER') {
+      return env.BUILD_USER
+    }
+    if (key == 'USERID') {
+      return env.BUILD_USER
+    }
+    if (key == 'EMAIL') {
+      return env.BUILD_USER_EMAIL
+    }
+  }
+}
+
