c2904f1f270b574209a2c1b72177b55fb5858dfb
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-12 19:28:07 +0530

    Finally solved bracket issues for infra and s3

--- terraform/aws/infra/s3/variables.tf
+++ terraform/aws/infra/s3/variables.tf
@@ -28,10 +28,10 @@ variable "description" {
   default = "test"
 }
 
-variable "aws_access_key" {
+variable "access_key" {
   default = "fake"
 }
-variable "aws_secret_key" {
+variable "secret_key" {
   default = "fake"
 }
 variable "region" {
