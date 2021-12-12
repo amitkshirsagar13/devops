f2c88ffb92461e70dd70bb176e6aa5211ea101c5
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-12 23:20:23 +0530

    Finally solved bracket issues for infra and s3

--- libraries/terraform/steps/terraformPlan.groovy
+++ libraries/terraform/steps/terraformPlan.groovy
@@ -5,7 +5,7 @@ void call(module, action, modulePath) {
       withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
                         string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
         if (module == "all") {
-          sh 'terragrunt run-all plan'
+          sh 'terragrunt run-all plan-all'
         } else {
           sh 'terragrunt plan'
         }
