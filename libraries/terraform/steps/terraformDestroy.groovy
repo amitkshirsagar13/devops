fe4c88f0388e62ba3b2ae0a74cca7b68c55db59e
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-12 23:18:09 +0530

    Finally solved bracket issues for infra and s3

--- libraries/terraform/steps/terraformDestroy.groovy
+++ libraries/terraform/steps/terraformDestroy.groovy
@@ -5,7 +5,7 @@ void call(module, action, modulePath) {
       withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
                         string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
         if (module == "all") {
-          sh 'terragrunt run-all destroy -auto-approve'
+          sh 'terragrunt run-all destroy-all -auto-approve'
         } else {
           sh 'terragrunt destroy -auto-approve'
         }
