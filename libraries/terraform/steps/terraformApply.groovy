22ebe8cc448d9a3f171db6f1c57ae0701feba7f6
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-13 08:53:24 +0530

    Finally solved bracket issues for infra and s3

--- libraries/terraform/steps/terraformApply.groovy
+++ libraries/terraform/steps/terraformApply.groovy
@@ -2,13 +2,10 @@ void call(module, action, modulePath) {
   stage("Terraform Apply") {
     println "Terraform ${action} for ${modulePath}"
     dir("${modulePath}") {
-      withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
-                        string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-        if (module == "all") {
-          sh 'terragrunt run-all apply -auto-approve'
-        } else {
-          sh 'terragrunt apply -auto-approve'
-        }
+      if (module == "all") {
+        sh 'terragrunt run-all apply -auto-approve'
+      } else {
+        sh 'terragrunt apply -auto-approve'
       }
     }
   }
--- libraries/terraform/steps/terraformPlan.groovy
+++ libraries/terraform/steps/terraformPlan.groovy
@@ -2,13 +2,10 @@ void call(module, action, modulePath) {
   stage("Terraform Plan") {
     println "Terraform ${action} for ${modulePath}"
     dir("${modulePath}") {
-      withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
-                        string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-        if (module == "all") {
-          sh 'terragrunt run-all plan'
-        } else {
-          sh 'terragrunt plan'
-        }
+      if (module == "all") {
+        sh 'terragrunt run-all plan'
+      } else {
+        sh 'terragrunt plan'
       }
     }
   }
