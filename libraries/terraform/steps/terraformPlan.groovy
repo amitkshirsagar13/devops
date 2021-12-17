19750b180390dad31fff9b9d1ec5c634dbd486f3
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-17 13:38:05 +0530

    timeout approval fixed

--- libraries/terraform/steps/terraformPlan.groovy
+++ libraries/terraform/steps/terraformPlan.groovy
@@ -1,13 +1,18 @@
 void call(module, action, modulePath) {
   stage("Terraform Plan") {
     println "Terraform ${action} for ${modulePath}"
+
+    planCmd = "plan"
+    if (module == "all") {
+      planCmd = "plan -destroy"
+    }
     withCredentials([string(credentialsId: "$KEY", variable: 'AWS_ACCESS_KEY_ID'),
                       string(credentialsId: "$SECRET", variable: 'AWS_SECRET_ACCESS_KEY')]) {
       dir("${modulePath}") {
         if (module == "all") {
-          sh 'terragrunt run-all plan -out=terraplan'
+          sh "terragrunt run-all $planCmd -out=terraplan"
         } else {
-          sh 'terragrunt plan -out=terraplan'
+          sh "terragrunt $planCmd -out=terraplan"
         }
       }
     }
