f78ac6276b53ee066142004a7f6ff5bc1bf1d799
Kshirsagar <trzxs9@splm.siemens.com>
2021-12-19 21:55:16 +0530

    kubernetes cli plugin added

--- libraries/ansible/steps/deploy_to.groovy
+++ libraries/ansible/steps/deploy_to.groovy
@@ -2,9 +2,7 @@ void call(appEnv) {
   // String nodeLabel = config.nodeLabel ?: { error "You need to define nodeLabel"}()
   stage("Deploy to ${appEnv.short_name}") {
     // node(nodeLabel) {
-      appEnv.ip_addresses.each { ip -> 
-        println "deploying to ${ip}"
-      }
+      appEnv.ip_addresses.each { ip -> println "deploying to ${ip}" }
     // }
   }
 }
