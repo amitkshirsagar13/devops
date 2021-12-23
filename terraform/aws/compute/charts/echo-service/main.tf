a54a2316be5ec5f9816340c69580c394d769a6a2
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 18:31:54 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/echo-service/main.tf
+++ terraform/aws/compute/charts/echo-service/main.tf
@@ -9,26 +9,26 @@ resource "helm_release" "echo-service" {
 
   set {
     name  = "namespace"
-    value = "${var.echo-service['namespace']}"
+    value = "${var.echo-service["namespace"]}"
   }
 
   set {
     name  = "replicaCount"
-    value = "${var.echo-service['replicaCount']}"
+    value = "${var.echo-service["replicaCount"]}"
   }
 
   set {
     name  = "autoscaling.minReplicas"
-    value = "${var.echo-service['minReplicas']}"
+    value = "${var.echo-service["minReplicas"]}"
   }
 
   set {
     name  = "autoscaling.maxReplicas"
-    value = "${var.echo-service['maxReplicas']}"
+    value = "${var.echo-service["maxReplicas"]}"
   }
 
   set {
     name  = "autoscaling.enabled"
-    value = "${var.echo-service['autoscaling']}"
+    value = "${var.echo-service["autoscaling"]}"
   }
 }
