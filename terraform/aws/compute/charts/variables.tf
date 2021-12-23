0d68f40bbb342a137358db32e87ba51a7d304db3
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 22:01:22 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/variables.tf
+++ terraform/aws/compute/charts/variables.tf
@@ -26,21 +26,29 @@ variable "maxReplicas" {
 
 
 variable "echo-service" {
-  type = string
-  // type = object({
-  //   namespace     = optional(string)
-  //   replicaCount  = optional(number)
-  //   autoscaling   = optional(bool)
-  //   minReplicas   = optional(number)
-  //   maxReplicas   = optional(number)
-  // })
+  // type = string
+  type = object({
+    namespace     = optional(string)
+    replicaCount  = optional(number)
+    autoscaling   = optional(bool)
+    minReplicas   = optional(number)
+    maxReplicas   = optional(number)
+  })
 }
 
 
+// locals {
+//   namespace     = jsondecode("${var.echo-service}")["namespace"]
+//   replicaCount  = jsondecode("${var.echo-service}")["replicaCount"]
+//   autoscaling   = jsondecode("${var.echo-service}")["autoscaling"]
+//   minReplicas   = jsondecode("${var.echo-service}")["minReplicas"]
+//   maxReplicas   = jsondecode("${var.echo-service}")["maxReplicas"]
+// }
+
 locals {
-  namespace     = jsondecode("${var.echo-service}")["namespace"]
-  replicaCount  = jsondecode("${var.echo-service}")["replicaCount"]
-  autoscaling   = jsondecode("${var.echo-service}")["autoscaling"]
-  minReplicas   = jsondecode("${var.echo-service}")["minReplicas"]
-  maxReplicas   = jsondecode("${var.echo-service}")["maxReplicas"]
+  namespace     = "${var.echo-service.namespace}"
+  replicaCount  = "${var.echo-service.replicaCount}"
+  autoscaling   = "${var.echo-service.autoscaling}"
+  minReplicas   = "${var.echo-service.minReplicas}"
+  maxReplicas   = "${var.echo-service.maxReplicas}"
 }
