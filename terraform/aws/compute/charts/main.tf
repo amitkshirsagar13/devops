836a5a4d464f664eba95e68a44b9094303db7f28
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 21:28:01 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/main.tf
+++ terraform/aws/compute/charts/main.tf
@@ -1,10 +1,30 @@
-  // provider "helm" {
-  //   kubernetes {
-  //     host     = "https://kubernetes.docker.internal:6443"
-  //     insecure = true
-  //   }
-  // }
+resource "helm_release" "echo-service" {
+  name       = "echo-service"
+  create_namespace = true
+  chart      = "tools/echo-service"
+  namespace  = "${var.namespace}"
 
-module "echo-service" {
-  source  = "./echo-service"
+  values = [
+    file("echo-service/values.yaml")
+  ]
+
+  set {
+    name  = "replicaCount"
+    value = "${var.replicaCount}"
+  }
+
+  set {
+    name  = "autoscaling.enabled"
+    value = "${var.autoscaling}"
+  }
+
+  set {
+    name  = "autoscaling.minReplicas"
+    value = "${var.minReplicas}"
+  }
+
+  set {
+    name  = "autoscaling.maxReplicas"
+    value = "${var.maxReplicas}"
+  }
 }
