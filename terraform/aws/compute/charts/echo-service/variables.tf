7e6beeae0d99006e5691260d1680d75138dcb0e6
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 19:49:05 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/echo-service/variables.tf
+++ terraform/aws/compute/charts/echo-service/variables.tf
@@ -1,46 +1,52 @@
-variable "namespace" {
-  type = string
-  default = jsondecode(var.echo-service)["namespace"]
+// variable "namespace" {
+//   type = string
+//   default = jsondecode(var.echo-service)["namespace"]
+// }
+
+// variable "replicaCount" {
+//   type = number
+//   default = jsondecode(var.echo-service)["replicaCount"]
+// }
+
+// variable "autoscaling" {
+//   type = bool
+//   default = jsondecode(var.echo-service)["autoscaling"]
+// }
+
+// variable "minReplicas" {
+//   type = number
+//   default = jsondecode(var.echo-service)["minReplicas"]
+// }
+
+// variable "maxReplicas" {
+//   type = number
+//   default = jsondecode(var.echo-service)["maxReplicas"]
+// }
+
+locals {
+  namespace     = jsondecode(var.echo-service)["namespace"]
+  replicaCount  = jsondecode(var.echo-service)["replicaCount"]
+  autoscaling   = jsondecode(var.echo-service)["autoscaling"]
+  minReplicas   = jsondecode(var.echo-service)["minReplicas"]
+  maxReplicas   = jsondecode(var.echo-service)["maxReplicas"]
 }
-
-variable "replicaCount" {
-  type = number
-  default = jsondecode(var.echo-service)["replicaCount"]
-}
-
-variable "autoscaling" {
-  type = bool
-  default = jsondecode(var.echo-service)["autoscaling"]
-}
-
-variable "minReplicas" {
-  type = number
-  default = jsondecode(var.echo-service)["minReplicas"]
-}
-
-variable "maxReplicas" {
-  type = number
-  default = jsondecode(var.echo-service)["maxReplicas"]
-}
-
-
 // terraform {
 //   experiments = [module_variable_optional_attrs]
 // }
 
-// variable "echo-service" {
-//   type = object({
-//     namespace     = optional(string)
-//     replicaCount  = optional(number)
-//     autoscaling   = optional(bool)
-//     minReplicas   = optional(number)
-//     maxReplicas   = optional(number)
-//   })
-//   default = {
-//     namespace     = "echo"
-//     replicaCount  = 1
-//     autoscaling   = false
-//     minReplicas   = 1
-//     maxReplicas   = 3
-//   }
-// }
+variable "echo-service" {
+  type = object({
+    namespace     = optional(string)
+    replicaCount  = optional(number)
+    autoscaling   = optional(bool)
+    minReplicas   = optional(number)
+    maxReplicas   = optional(number)
+  })
+  // default = {
+  //   namespace     = "echo"
+  //   replicaCount  = 1
+  //   autoscaling   = false
+  //   minReplicas   = 1
+  //   maxReplicas   = 3
+  // }
+}
