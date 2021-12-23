2f0a4d25ffe7cf4d8c8d527c65413bfb515f65b7
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 22:13:36 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/variables.tf
+++ terraform/aws/compute/charts/variables.tf
@@ -1,22 +1,22 @@
-// terraform {
-//   experiments = [module_variable_optional_attrs] 
-// }
+terraform {
+  experiments = [module_variable_optional_attrs] 
+}
 
 
-// variable "echo-service" {
-//   type = object({
-//     namespace     = optional(string)
-//     replicaCount  = optional(number)
-//     autoscaling   = optional(bool)
-//     minReplicas   = optional(number)
-//     maxReplicas   = optional(number)
-//   })
-// }
+variable "echo-service" {
+  type = object({
+    namespace     = optional(string)
+    replicaCount  = optional(number)
+    autoscaling   = optional(bool)
+    minReplicas   = optional(number)
+    maxReplicas   = optional(number)
+  })
+}
 
-// locals {
-//   namespace     = "${var.echo-service.namespace}"
-//   replicaCount  = "${var.echo-service.replicaCount}"
-//   autoscaling   = "${var.echo-service.autoscaling}"
-//   minReplicas   = "${var.echo-service.minReplicas}"
-//   maxReplicas   = "${var.echo-service.maxReplicas}"
-// }
+locals {
+  namespace     = "${var.echo-service.namespace}"
+  replicaCount  = "${var.echo-service.replicaCount}"
+  autoscaling   = "${var.echo-service.autoscaling}"
+  minReplicas   = "${var.echo-service.minReplicas}"
+  maxReplicas   = "${var.echo-service.maxReplicas}"
+}
