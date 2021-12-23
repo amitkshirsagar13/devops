4221d7ef292eb009d2c3ad0139a06c914137fa31
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 18:54:19 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/main.tf
+++ terraform/aws/compute/charts/main.tf
@@ -1,3 +1,10 @@
+  provider "helm" {
+    kubernetes {
+      host     = "https://kubernetes.docker.internal:6443"
+      insecure = true
+    }
+  }
+  
 module "echo-service" {
   source  = "./echo-service"
 }
