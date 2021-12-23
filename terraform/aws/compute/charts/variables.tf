66f029401a524e8f3f53bdeff03c9f9940b2867e
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-12-23 21:53:37 +0530

    terragrunt configuration for helm

--- terraform/aws/compute/charts/variables.tf
+++ terraform/aws/compute/charts/variables.tf
@@ -23,31 +23,6 @@ variable "maxReplicas" {
   default = 3
 }
 
-variable "namespace" {
-  type = string
-  default = "echo"
-}
-
-variable "replicaCount" {
-  type = number
-  default = 1
-}
-
-variable "autoscaling" {
-  type = bool
-  default = false
-}
-
-variable "minReplicas" {
-  type = number
-  default = 1
-}
-
-variable "maxReplicas" {
-  type = number
-  default = 3
-}
-
 
 
 variable "echo-service" {
