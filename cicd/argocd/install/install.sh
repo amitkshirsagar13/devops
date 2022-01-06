ac2ba34bc27ba4ff51fd919328535f6dfb3530ee
Kshirsagar <trzxs9@splm.siemens.com>
2022-01-06 14:38:02 +0530

    added code

--- cicd/argocd/install/install.sh
+++ cicd/argocd/install/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kubectl create namespace argocd
 kubectl apply -n argocd -f install.yaml
-
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
