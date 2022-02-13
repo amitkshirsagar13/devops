142e8e071b0367673475cdd559b65ec9284e37b5
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2022-02-13 12:46:04 +0530

    Update tools.bat

--- local/tools.bat
+++ local/tools.bat
@@ -1,1 +1,4 @@
 choco install ptime
+curl https://github.com/amitkshirsagar13/devops/blob/main/local/ab.exe -o ab.exe
+curl https://github.com/amitkshirsagar13/devops/blob/main/local/abs.exe -o abs.exe
+curl https://github.com/amitkshirsagar13/devops/blob/main/local/openssl.exe -o openssl.exe
