5e9504f3034548075f635dbd51737f7a36c20e8b
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2022-02-13 12:52:39 +0530

    Update tools.bat

--- local/tools.bat
+++ local/tools.bat
@@ -1,4 +1,4 @@
-choco install ptime curl apache-httpd --force
+choco install ptime curl apache-httpd lens --force
 curl https://github.com/amitkshirsagar13/devops/blob/main/local/ab.exe -o ab.exe
 curl https://github.com/amitkshirsagar13/devops/blob/main/local/abs.exe -o abs.exe
 curl https://github.com/amitkshirsagar13/devops/blob/main/local/openssl.exe -o openssl.exe
